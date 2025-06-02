86058ec70494f7d3a751f0306527d2a054beb332
qazit <agentsniffer@gmail.com>
2025-06-02 21:48:51 +0800

    upd

--- src/Main.java
+++ src/Main.java
@@ -426,8 +426,16 @@ public class Main {
     }
 
     static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try {
+            String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     static void waitForInput() {
